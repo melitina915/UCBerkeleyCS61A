cd6c122d7862d736840114f7bf4077f6777da274
melitina915 <90691824+melitina915@users.noreply.github.com>
2022-09-04 15:40:34 +0900

    Create questions.scm

---
+++ project/scheme/questions.scm
@@ -0,0 +1,32 @@
+(define (caar x) (car (car x)))
+(define (cadr x) (car (cdr x)))
+(define (cdar x) (cdr (car x)))
+(define (cddr x) (cdr (cdr x)))
+
+;; Problem 15
+;; Returns a list of two-element lists
+(define (enumerate s)
+  ; BEGIN PROBLEM 15
+  (define (num i s)
+     (if (null? s) nil
+        (cons (list i (car s)) (num (+ 1 i) (cdr s)))
+     )
+  )
+ (num 0 s)
+  )
+  ; END PROBLEM 15
+
+;; Problem 16
+
+;; Merge two lists LIST1 and LIST2 according to ORDERED? and return
+;; the merged lists.
+(define (merge ordered? list1 list2)
+  ; BEGIN PROBLEM 16
+  (cond ((null? list1) list2) ((null? list2) list1)
+        ((ordered? (car list1) (car list2))
+            (cons (car list1) (merge ordered? (cdr list1) list2)))
+        (else
+            (cons (car list2) (merge ordered? list1 (cdr list2)))))
+  )
+  ; END PROBLEM 16
+
